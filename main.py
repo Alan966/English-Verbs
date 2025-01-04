82d4eb93c6fe04bb67dfe50f260372f37d111a68
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2025-01-04 12:57:56 -0600

    refact: give discount users with the same artist that me

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def main():
     print("We'll start this game")
     name = get_name()
-
     #Ask the user their age
     age = get_age()
     if verify_age(age) == False:
@@ -9,8 +8,8 @@ def main():
         return
     print("You're old enough to play this game")
     bank_account = get_bank_account()
-    discount_code = get_discountCode()
-    if could_play_game(discount_code, bank_account) == False:
+    favorite_artis = get_favorite_artist()
+    if could_play_game(favorite_artis, bank_account) == False:
         print("You don't have enough money to play this game")
         return
     print("You can play this game")
@@ -28,26 +27,32 @@ def get_bank_account():
     return round(float(bank_account), 2)
 def verify_bank_account(bank_account):
     return bank_account >= 100
-def get_discountCode():
-    discountCode = input("Do you have a first discount code? ")
-    return discountCode
-def verify_discountCode(discountCode):
-    return discountCode == "DRAKE" or discountCode == "PARTYNEXTDOOR" or discountCode =="TYLOR,THECREATOR"
-def verify_discountCodes_with_percentage(discountCode):
-    match discountCode:
+def get_favorite_artist():
+    favorite_artist = input("Who is your favorite Artist? ")
+    return favorite_artist.strip().upper()
+def verify_favorite_artist(artist: str):
+    if(artist.startswith("DRAKE")):
+        return "DRAKE"
+    elif(artist.startswith("PARTYNEXTDOOR")):
+        return "PARTYNEXTDOOR"
+    elif(artist.startswith("TYLOR THE CREATOR")):
+        return "TYLOR THE CREATOR"
+def verify_discountCodes_by_artist(artsit: str):
+    match artsit:
         case "DRAKE":
             return 0.25
         case "PARTYNEXTDOOR":
             return 0.1
-        case "TYLOR,THECREATOR":
+        case "TYLOR THE CREATOR":
             return 0.05
         case _:
             return
 def could_play_game(discount_code, bank_account):
-    if verify_discountCode(discount_code) == False:
+    formmated_artist = verify_favorite_artist(discount_code)
+    if formmated_artist == None:
       return verify_bank_account(bank_account)
     else:
-      discount = verify_discountCodes_with_percentage(discount_code) or 0
+      discount = verify_discountCodes_by_artist(formmated_artist) or 0
       return bank_account >= 100 * (1 - discount)
 
 if __name__ == "__main__":
